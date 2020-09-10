ed309af1e41d46883f076b46d4bb46d079877134
Kayla Budzeak <kaylahmg@gmail.com>
2020-09-10 16:39:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,5 @@
 def reverse_each_word(sentence)
-  new_sentence = []
-  sentence.split.each do |word|
+  sentence.split.collect do |word|
     word = word.reverse
-    new_sentence << word
   end
-  new_sentence.join(" ")
 end
